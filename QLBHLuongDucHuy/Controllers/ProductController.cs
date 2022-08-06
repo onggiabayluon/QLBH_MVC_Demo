b5e8c3d6834ed55a0e451de684f2d638bd63595e
ducchuy <livipst2@gmail.com>
2022-08-06 15:01:55 +0700

    f

---
+++ QLBHLuongDucHuy/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLBHLuongDucHuy.Models;
+
+
+namespace QLBHLuongDucHuy.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        NorthwindDataContext da = new NorthwindDataContext();
+        // List: Product
+        public ActionResult ListProduct()
+        {
+            List<Product> products = da.Products.Select(p => p).ToList();
+
+
+            return View(products);
+        }
+
+    }
+}
---
+++ QLBHLuongDucHuy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QLBHLuongDucHuy.MvcApplication" Language="C#" %>
